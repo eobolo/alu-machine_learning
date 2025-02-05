fb3b8a7df0367220be11ac75fe1be603448dcf85
emmanuel obolo <e.obolo@alustudent.com>
2025-02-05 18:46:14 +0000

    regularization

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-This is batch normalization
+Created on Tue Jan 12 18:12:52 2021
+
+@author: Robinson Montes
 """
-import numpy as np
+import tensorflow as tf
 
 
-def batch_norm(Z, gamma, beta, epsilon):
+def create_batch_norm_layer(prev, n, activation):
     """
-    Function that normalizes an unactivated output of a NN using
-    batch normalization
+    Function that creates a batch normalization layer for a NN in tensorflow:
     Arguments:
-     - Z is a numpy.ndarray of shape (m, n) that should be normalized
-         * m is the number of data points
-         * n is the number of features in Z
-     - gamma is a numpy.ndarray of shape (1, n) containing the scales
-        used for batch normalization
-     - beta is a numpy.ndarray of shape (1, n) containing the offsets
-        used for batch normalization
-     - epsilon is a small number used to avoid division by zero
+     - prev is the activated output of the previous layer
+     - n is the number of nodes in the layer to be created
+     - activation is the activation function that should be used on
+        the output of the layer
     Returns:
-    The normalized Z matrix
+     A tensor of the activated output for the layer
     """
 
-    mt = Z.mean(0)
-    vt = Z.var(0)
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(units=n, kernel_initializer=init)
 
-    Zt = (Z - mt) / (vt + epsilon) ** (1/2)
-    normalized_Z = gamma * Zt + beta
+    z = layer(prev)
 
-    return normalized_Z
+    mt, vt = tf.nn.moments(z, [0])
+    beta = tf.Variable(tf.zeros([z.get_shape()[-1]]))
+    gamma = tf.Variable(tf.ones([z.get_shape()[-1]]))
+    zt = tf.nn.batch_normalization(z, mt, vt, beta, gamma, 1e-8)
+    y_pred = activation(zt)
+
+    return y_pred

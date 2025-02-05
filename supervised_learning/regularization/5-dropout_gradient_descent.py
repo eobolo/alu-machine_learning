9321dbde1c7d117c3956f75ff621fead2615e4fd
emmanuel obolo <e.obolo@alustudent.com>
2025-02-05 18:14:28 +0000

    regularization

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
-Defines function that updates the weights with Dropout regularization
-using gradient descent
+Gradient Descent with Dropout
 """
 
 import numpy as np
@@ -9,51 +8,43 @@ import numpy as np
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
-    Updates the weights with Dropout regularization using gradient descent
-
-    parameters:
-        Y [one-hot numpy.ndarray of shape (classes, m)]:
-            contains the correct labels for the data
-            classes: number of classes
-            m: number of data points
-        weights [dict]:
-            contains the weights and biases of the network
-        cache [dict]:
-            contains the outputs and dropout masks of each layer
-        alpha [float]:
-            learning rate
-        keep_prob [float]:
-            the probability that a node will be kept
-        L [int]:
-            number of layers in the network
-
-    all layers should use the tanh activation function except last
-    last layer should use softmax activation function
-
-    the weights of the network should be updated in place
+    Function that updates the weights of a NN with Dropout regularization
+    using gradient descent
+    Arguments:
+     - Y is a one-hot numpy.ndarray of shape (classes, m) that contains
+         the correct labels for the data
+         * classes is the number of classes
+         * m is the number of data points
+     - weights is a dictionary of the weights and biases of the NN
+     - cache is a dictionary of the outputs and dropout masks of
+        each layer of the NN
+     - alpha is the learning rate
+     - keep_prob is the probability that a node will be kept
+     - L is the number of layers of the network
     """
     m = Y.shape[1]
-    back = {}
-    for index in range(L, 0, -1):
-        A = cache["A{}".format(index - 1)]
-        if index == L:
-            back["dz{}".format(index)] = (cache["A{}".format(index)] - Y)
-            dz = back["dz{}".format(index)]
+    Al = cache['A' + str(L)]
+    dAl = Al - Y
 
+    for layer in reversed(range(1, L + 1)):
+        w_key = 'W' + str(layer)
+        b_key = 'b' + str(layer)
+        Al_key = 'A' + str(layer)
+        Al1_key = 'A' + str(layer - 1)
+        D_key = 'D' + str(layer)
+
+        Al = cache[Al_key]
+        gld = 1 - np.power(Al, 2)
+        if layer == L:
+            dZl = dAl
         else:
-            dz_prev = back["dz{}".format(index + 1)]
-            A_current = cache["A{}".format(index)]
-            back["dz{}".format(index)] = (
-                np.matmul(W_prev.transpose(), dz_prev) *
-                (A_current * (1 - A_current)))
-            dz = back["dz{}".format(index)]
-            dz *= cache["D{}".format(index)]
-            dz /= keep_prob
+            dZl = dAl * gld
+            dZl *= cache[D_key] / keep_prob
 
-        dW = (1 / m) * (np.matmul(dz, A.transpose()))
-        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-        W_prev = weights["W{}".format(index)]
-        weights["W{}".format(index)] = (
-            weights["W{}".format(index)] - (alpha * dW))
-        weights["b{}".format(index)] = (
-            weights["b{}".format(index)] - (alpha * db))
+        Wl = weights[w_key]
+        Al1 = cache[Al1_key]
+        dWl = (1 / m) * np.matmul(dZl, Al1.T)
+        dbl = (1 / m) * np.sum(dZl, axis=1, keepdims=True)
+        dAl = np.matmul(Wl.T, dZl)
+        weights[w_key] = weights[w_key] - alpha * dWl
+        weights[b_key] = weights[b_key] - alpha * dbl

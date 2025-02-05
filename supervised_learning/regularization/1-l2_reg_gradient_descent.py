dee7e9add476838ee9631ff723266b1d8574f04b
emmanuel obolo <e.obolo@alustudent.com>
2025-02-05 18:20:51 +0000

    regularization

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
-Defines a function that updates the weights and biases
-using gradient descent with L2 Regularization
+Gradient Descent with L2 Regularization
 """
 
 import numpy as np
@@ -9,43 +8,29 @@ import numpy as np
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """
-    Updates weights and biases using gradient descent with L2 regularization
+    Function that updates the weights and biases of a NN using gradient descent
+    with L2 regularization
+    Arguments:
+     - Y is a one-hot numpy.ndarray of shape (classes, m) that contains the
+       correct labels for the data
+        * classes is the number of classes
+        * m is the number of data points
+     - weights is a dictionary of the weights and biases of the NN
+     - cache is a dictionary of the outputs of each layer of the NN
+     - alpha is the learning rate
+     - lambtha is the L2 regularization parameter
+     - L is the number of layers of the network
+     """
 
-    parameters:
-        Y [one-hot numpy.ndarray of shape (classes, m)]:
-            contains the correct labels for the data
-            classes: number of classes
-            m: number of data points
-        weights [dict]: dictionary of weights and biases for the network
-        cache [dict]: dictionary of the outputs of each layer of the network
-        alpha [float]: learning rate
-        lambtha: the regularization parameter
-        L: the number of layers in the neural network
-
-    Neural network using tanh activations on each layer except the last.
-    Last layer uses softmax activation.
-    """
-    m = Y.shape[1]
-    back = {}
-    for index in range(L, 0, -1):
-        A = cache["A{}".format(index - 1)]
-        if index == L:
-            back["dz{}".format(index)] = (cache["A{}".format(index)] - Y)
-        else:
-            dz_prev = back["dz{}".format(index + 1)]
-            A_current = cache["A{}".format(index)]
-            back["dz{}".format(index)] = (
-                np.matmul(W_prev.transpose(), dz_prev) *
-                (A_current * (1 - A_current)))
-        dz = back["dz{}".format(index)]
-        dW = (1 / m) * (
-            (np.matmul(dz, A.transpose())) + (
-                lambtha * weights["W{}".format(index)]))
-        db = (1 / m) * (
-            (np.sum(dz, axis=1, keepdims=True)) + (
-                lambtha * weights["b{}".format(index)]))
-        W_prev = weights["W{}".format(index)]
-        weights["W{}".format(index)] = (
-            weights["W{}".format(index)] - (alpha * dW))
-        weights["b{}".format(index)] = (
-            weights["b{}".format(index)] - (alpha * db))
+    weights_copy = weights.copy()
+    dz = cache["A" + str(L)] - Y
+    for i in range(L, 0, -1):
+        A = cache["A" + str(i - 1)]
+        w = "W" + str(i)
+        b = "b" + str(i)
+        dw = (1 / len(Y[0])) * np.matmul(dz, A.T) + (
+            lambtha * weights[w]) / len(Y[0])
+        db = (1 / len(Y[0])) * np.sum(dz, axis=1, keepdims=True)
+        weights[w] = weights[w] - alpha * dw
+        weights[b] = weights[b] - alpha * db
+        dz = np.matmul(weights_copy["W" + str(i)].T, dz) * (1 - A * A)

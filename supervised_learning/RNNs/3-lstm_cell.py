cff5202f93b48dfceb8a089472960ceb851289d6
Obolo Emmanuel <e.obolo@alustudent.com>
2025-05-14 00:10:58 +0200

    updates

--- supervised_learning/RNNs/3-lstm_cell.py
+++ supervised_learning/RNNs/3-lstm_cell.py
@@ -1,123 +1,100 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
-Defines the class LSTMCell that represents an LSTM unit
+3-lstm_cell.py question
 """
-
-
 import numpy as np
 
 
-class LSTMCell:
+# Function
+def sigmoid(x):
     """
-    Represents a LSTM unit
-
-    class constructor:
-        def __init__(self, i, h, o)
-
-    public instance attributes:
-        Wf: forget gate weights
-        bf: forget gate biases
-        Wu: update gate weights
-        bu: update gate biases
-        Wc: intermediate cell state weights
-        bc: intermediate cell state biases
-        Wo: output gate weights
-        bo: output gate biases
-        Wy: output weights
-        by: output biases
-
-    public instance methods:
-        def forward(self, h_prev, c_prev, x_t):
-            performs forward propagation for one time step
+    Sigmoid function
     """
+    sigmoid = 1 / (1 + np.exp(-x))
+
+    return sigmoid
+
+
+class LSTMCell():
+    """
+    Class LSTMCell that represents an LSTM unit
+    """
+
     def __init__(self, i, h, o):
         """
-        Class constructor
+        class constructor
 
-        parameters:
-            i: dimensionality of the data
-            h: dimensionality of the hidden state
-            o: dimensionality of the outputs
+        Argumetns:
+         - i is the dimensionality of the data
+         - h is the dimensionality of the hidden state
+         - o is the dimensionality of the outputs
 
-        creates public instance attributes:
-            Wf: forget gate weights
-            bf: forget gate biases
-            Wu: update gate weights
-            bu: update gate biases
-            Wc: intermediate cell state weights
-            bc: intermediate cell state biases
-            Wo: output gate weights
-            bo: output gate biases
-            Wy: output weights
-            by: output biases
-
-        weights should be initialized using random normal distribution
-        weights will be used on the right side for matrix multiplication
-        biases should be initiliazed as zeros
+        Public instance attributes
+         Wf, Wu, Wc, Wo, Wy, bf, bu, bc, bo, by that represent
+         the weights and biases of the cell
+         - Wf and bf are for the forget gate
+         - Wu and bu are for the update gate
+         - Wc and bc are for the intermediate cell state
+         - Wo and bo are for the output gate
+         - Wy and by are for the outputs
         """
-        self.bf = np.zeros((1, h))
-        self.bu = np.zeros((1, h))
-        self.bc = np.zeros((1, h))
-        self.bo = np.zeros((1, h))
-        self.by = np.zeros((1, o))
+
+        # Weights
         self.Wf = np.random.normal(size=(h + i, h))
         self.Wu = np.random.normal(size=(h + i, h))
         self.Wc = np.random.normal(size=(h + i, h))
         self.Wo = np.random.normal(size=(h + i, h))
         self.Wy = np.random.normal(size=(h, o))
+        # Bias
+        self.bf = np.zeros((1, h))
+        self.bu = np.zeros((1, h))
+        self.bc = np.zeros((1, h))
+        self.bo = np.zeros((1, h))
+        self.by = np.zeros((1, o))
 
+    # Function
     def softmax(self, x):
         """
-        Performs the softmax function
-
-        parameters:
-            x: the value to perform softmax on to generate output of cell
-
-        return:
-            softmax of x
+        Function to compute softmax values for each sets of scores in x
         """
-        e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
-        softmax = e_x / e_x.sum(axis=1, keepdims=True)
-        return softmax
 
-    def sigmoid(self, x):
-        """
-        Performs the sigmoid function
+        x_max = np.max(x, axis=1, keepdims=True)
+        e_x = np.exp(x - x_max)
 
-        parameters:
-            x: the value to perform sigmoid on
+        return e_x / np.sum(e_x, axis=1, keepdims=True)
 
-        return:
-            sigmoid of x
-        """
-        sigmoid = 1 / (1 + np.exp(-x))
-        return sigmoid
-
+    # public instance method
     def forward(self, h_prev, c_prev, x_t):
         """
-        Performs forward propagation for one time step
+        Public instance method that performs forward propagation
+        for one time step
 
-        parameters:
-            h_prev [numpy.ndarray of shape (m, h)]:
-                contains previous hidden state
-                m: the batch size for the data
-                h: dimensionality of hidden state
-            c_prev [numpy.ndarray of shape (m, h)]:
-                contains previous cell state
-                m: the batch size for the data
-                h: dimensionality of hidden state
-            x_t [numpy.ndarray of shape (m, i)]:
-                contains data input for the cell
-                m: the batch size for the data
-                i: dimensionality of the data
+        Arguments:
+         - x_t is a numpy.ndarray of shape (m, i) that contains
+            the data input for the cell
+            * m is the batche size for the data
+         - h_prev is a numpy.ndarray of shape (m, h) containing
+            the previous hidden state
+         - c_prev is a numpy.ndarray of shape (m, h) containing
+            the previous cell state
 
-        output of the cell should use softmax activation function
-
-        returns:
-            h_next, c_next, y:
-            h_next: the next hidden state
-            c_next: the next cell state
-            y: the output of the cell
+        Returns:
+         h_next, c_next, y
+            - h_next is the next hidden state
+            - c_next is the next cell state
+            - y is the output of the cell
         """
-        concatenation = np.concatenate((h_prev, x_t), axis=1)
-        u_gate = self.sigmoid
+
+        matrix = np.concatenate((h_prev, x_t), axis=1)
+        u_t = sigmoid(np.matmul(matrix, self.Wu) + self.bu)
+        f_t = sigmoid(np.matmul(matrix, self.Wf) + self.bf)
+        o_t = sigmoid(np.matmul(matrix, self.Wo) + self.bo)
+
+        prime_c = np.tanh(np.matmul(matrix, self.Wc) + self.bc)
+        c_next = f_t * c_prev + u_t * prime_c
+        h_next = o_t * np.tanh(c_next)
+
+        y = self.softmax(np.matmul(h_next, self.Wy) + self.by)
+
+        return h_next, c_next, y
